89c8f9fc4f51792760000a127ac10c9702700be9
x <noemail@no.io>
2022-01-19 12:56:25 +0100

    Fixes bug when product runs out of stock. Adds warning messages to pay button.

--- src/app/service/vending-machine.service.ts
+++ src/app/service/vending-machine.service.ts
@@ -77,7 +77,39 @@ export class VendingMachineService {
    * @returns true if there's a selection and credit is equal or greater than its price
    */
   public canPay(): boolean{
-    return !(this._selectedSnack instanceof Initial) && this._credit >= this._selectedSnack.price
+    return this._credit >= this._selectedSnack.price
+  }
+
+  /**
+   * 
+   * @returns true if current selection has at least one unit left for sale
+   */
+  public hasStock(): boolean{
+    return this._selectedSnack.stock > 0
+  }
+
+  /**
+   * 
+   * @returns true if there is a valid user selection
+   */
+  public hasValidSelection(): boolean{
+    return !(this._selectedSnack instanceof Initial)
+  }
+
+  /**
+   * 
+   * @returns true if the user can pay a valid selected product
+   */
+  public isShowNoCreditMessage(): boolean{
+    return this.hasValidSelection() && !this.canPay()
+  }
+
+  /**
+   * 
+   * @returns true if the user has selected a product which currently has stock
+   */
+  public isShowNoStockMessage(): boolean{
+    return this.hasValidSelection() && !this.hasStock()
   }
 
   public pay(): void{
